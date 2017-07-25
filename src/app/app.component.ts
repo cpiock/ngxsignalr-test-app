831a5f32d9fb4d0002ace23b1775525735525919
Christoph Piock-Ellena <christoph.piock-ellena@systems.bz>
2017-07-25 17:15:54 +0200

    creates a signalr test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Route, ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+
+import { SignalrChannelService, ConnectionState, ChannelEvent} from './signalrchannel.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  // An internal "copy" of the connection state stream used because
+    //  we want to map the values of the original stream. If we didn't
+    //  need to do that then we could use the service's observable
+    //  right in the template.
+    connectionState$: Observable<string>;
+
+    constructor(
+        private signalrChannelService: SignalrChannelService) {
+        // Let's wire up to the signalr observables
+        //
+        this.connectionState$ = this.signalrChannelService.connectionState$
+            .map((state: ConnectionState) => { return ConnectionState[state]; });
+
+        this.signalrChannelService.error$.subscribe(
+            (error: any) => { console.warn(error); },
+            (error: any) => { console.error('errors$ error', error); }
+        );
+
+        // Wire up a handler for the starting$ observable to log the
+        //  success/fail result
+        //
+        this.signalrChannelService.starting$.subscribe(
+            () => { console.log('signalr service has been started'); },
+            () => { console.warn('signalr service failed to start!'); }
+        );
+    }
+
+
+    ngOnInit() {
+          this.signalrChannelService.start();
+
+          this.signalrChannelService.sub('e0e7a11c-967a-43e4-9d10-7984bc96d9b4').subscribe(
+            (x: ChannelEvent) => {
+                console.log(x.Data);
+            },
+            (error: any) => {
+                console.warn('Attempt to join channel failed!', error);
+            }
+        )
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import {SignalrChannelService, ChannelConfig, SignalrWindow, channelConfig} from './signalrchannel.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ SignalrChannelService,
+    { provide: SignalrWindow, useValue: window },
+    { provide: 'channel.config', useValue: channelConfig}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
